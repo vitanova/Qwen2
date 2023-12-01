331d74c83584debdf5426b81182b5ffb60d4941e
fangdawang <vitanova1001@gmail.com>
2023-12-01 23:38:00 +0800

    fxdp

--- finetune/finetune_qlora_single_gpu.sh
+++ finetune/finetune_qlora_single_gpu.sh
@@ -5,7 +5,7 @@ DIR=`pwd`
 MODEL="Qwen/Qwen-7B-Chat-Int4" # Set the path if you do not want to load from huggingface directly
 # ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
 # See the section for finetuning in README for more information.
-DATA="path_to_data"
+DATA="finetune/data.json"
 
 function usage() {
     echo '
